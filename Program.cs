dcbe4521f7e5ee7961937ec71ede2e64ca9f1088
Hoang <luumanhlapdi@gmail.com>
2018-11-14 10:53:34 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadandXDoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sources =
+            {
+                "https://dantri.com.vn/trangchu.rss",
+                "https://www.24h.com.vn/upload/rss/trangchu24h.rss",
+                "http://vietnamnet.vn/rss/home.rss",
+                "http://gamek.vn/trang-chu.rss"
+            };
+
+            string[] keywords = { "tình" };
+
+            List<RssAnalyzer> analyzers = new List<RssAnalyzer>();
+
+            foreach (string url in sources)
+            {
+                analyzers.Add(new RssAnalyzer(url, keywords));
+            }
+
+            //WAIT
+            foreach (var item in analyzers)
+            {
+                item.Join();
+            }
+            //WRITE
+            foreach (var item in analyzers)
+            {
+                foreach (string url in item.Links)
+                {
+                    Console.WriteLine(url);
+                    Process.Start(url);
+                    Thread.Sleep(500);
+                }
+            }
+            Console.WriteLine("END");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RssAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ThreadandXDoc
+{
+    class RssAnalyzer
+    {
+        string[] keywords;
+        string url;
+        Thread thread;
+
+        public List<string> Links { get; private set; }
+
+        public RssAnalyzer(string url, string[] keywords)
+        {
+            this.url = url;
+            this.keywords = keywords;
+            thread = new Thread(GetLink);
+            thread.Start();
+        }
+
+        public void Join()
+        {
+            thread.Join(); // Blocking method - Wait until the previous thread fullfill
+        }
+
+        public void GetLink()
+        {
+            XDocument document = XDocument.Load(url);
+            var items = document.Element("rss").Element("channel").Elements("item");
+            var neededItem = items.Where(
+                item => keywords.All(
+                    word => item.Element("description").Value.ToLower().Contains(word)));
+
+            Links = neededItem.Select(node => node.Element("link").Value).ToList();
+        }
+    }
+}
